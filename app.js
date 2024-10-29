86812f478ae6c1cf8d361c50019a3e2fd6837839
Patience0709 <patience0709@gmail.com>
2024-10-29 20:58:11 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/restaurant-list')
+})
+
+app.get('/restaurant-list', (req, res) => {
+  res.send('listing restaurants')
+})
+
+app.get('/restaurant-list/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
 })
 
 app.listen(port, () => {
