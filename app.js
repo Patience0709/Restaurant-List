e537ddd98a4a9077eb1949eba7c9d6c47006cbc9
Patience0709 <patience0709@gmail.com>
2024-11-04 20:45:34 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword?.trim() || ''//定義undefind為空字串
+  const matchedRestaurant = restaurants.filter((restaurant) => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+  restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+)
+  res.render('index', { restaurants: matchedRestaurant, keyword })
+})
+
 app.get('/restaurants', (req, res) => {
   res.render('index', { restaurants })
 })
